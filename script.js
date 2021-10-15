fe330ae8e0bb3a2cbf520e8ff8e08a57e0608a67
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-10-15 13:24:12 -0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const stepAnimation = () => {
   const sprite = document.querySelector('#sprite')
-
+  sprite.setAttribute('class', 'sprite-idle-right')
   document.addEventListener('keydown', (e)=>{
     console.log(e)
 
@@ -12,13 +12,49 @@ const stepAnimation = () => {
       sprite.setAttribute('class', 'sprite-run-left')
     }
 
-    if(e.code ==='KeyZ'){
-      sprite.setAttribute('class', 'sprite-attack')
+    if(e.code ==='KeyZ' && sprite.getAttribute('class') === 'sprite-idle-right'){
+      sprite.setAttribute('class', 'sprite-attack-right')
     }
 
-    if(e.code ==='KeyX'){
-      sprite.setAttribute('class', 'sprite-attack-2')
+    if(e.code ==='KeyZ' && sprite.getAttribute('class') === 'sprite-idle-left'){
+      sprite.setAttribute('class', 'sprite-attack-left')
     }
+
+    if(e.code ==='KeyX' && sprite.getAttribute('class') === 'sprite-idle-right'){
+      sprite.setAttribute('class', 'sprite-attack-2-right')
+    }
+
+    if(e.code ==='KeyX' && sprite.getAttribute('class') === 'sprite-idle-left'){
+      sprite.setAttribute('class', 'sprite-attack-2-left')
+    }
+  })
+
+  document.addEventListener('keyup', (e)=>{
+    console.log(e)
+    if(e.key === 'ArrowRight'){
+      sprite.setAttribute('class', 'sprite-idle-right')
+    }
+    if(e.key === 'ArrowLeft'){
+      sprite.setAttribute('class', 'sprite-idle-left')
+    }
+
+    if(e.code ==='KeyX' && sprite.getAttribute('class') === 'sprite-attack-2-right'){
+      sprite.setAttribute('class', 'sprite-idle-right')
+    }
+    
+    if(e.code ==='KeyX' && sprite.getAttribute('class') === 'sprite-attack-2-left'){
+      sprite.setAttribute('class', 'sprite-idle-left')
+    }
+
+    if(e.code ==='KeyZ' && sprite.getAttribute('class') === 'sprite-attack-right'){
+      sprite.setAttribute('class', 'sprite-idle-right')
+    }
+
+    if(e.code ==='KeyZ' && sprite.getAttribute('class') === 'sprite-attack-left'){
+      sprite.setAttribute('class', 'sprite-idle-left')
+    }
+
+    
   })
 }
 stepAnimation()
